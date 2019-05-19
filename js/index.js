90f43a5f942f559d614a040ac980a99cba055af6
renjiefang <1285571335@qq.com>
2019-05-19 16:48:50 +0800

    water first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/**
+ * @author renjiefang
+ * @date  2019-05-15 10:34
+ */
+
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth>=750){
+                // 这里的640 取决于设计稿的宽度
+                docEl.style.fontSize = '100px';
+            }else{
+                docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
